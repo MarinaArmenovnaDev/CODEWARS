c0cacadf1fe48f4950c72228528b28527b07df16
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2024-09-01 23:46:44 +0300

    4th commit

---
+++ 8kyu/ convert-boolean-values-to-strings-yes-or-no.js
@@ -0,0 +1,7 @@
+//❓ Завершите метод, который принимает логическое значение и возвращает "Yes"строку для true или "No"строку для false.
+
+// ✅ SOLUTION:
+
+function boolToWord( bool ){
+    return bool ? "Yes" : "No"
+  }
