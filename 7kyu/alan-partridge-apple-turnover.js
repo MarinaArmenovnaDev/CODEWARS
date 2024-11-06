01a385663d542a40e3a37157f1d91109ac7c7c8c
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2024-11-07 00:00:28 +0300

    fix1

---
+++ 7kyu/alan-partridge-apple-turnover.js
@@ -0,0 +1,8 @@
+// ❓Ваша задача проста: если xквадрат больше 1000, вернуть It's hotter than the sun!!, иначе вернутьHelp yourself to a honeycomb Yorkie for the glovebox.
+
+// Примечание: Входные данные могут быть положительным целым числом (или строкой для нетипизированных языков).
+
+// ✅ SOLUTION:
+function apple(x){
+return x*x > 1000 ? "It's hotter than the sun!!" : "Help yourself to a honeycomb Yorkie for the glovebox."
+}
