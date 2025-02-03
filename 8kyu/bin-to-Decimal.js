cdf15bfe45c2a6fc357d77b52b63f027e3f674d4
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2025-02-03 11:47:37 +0300

    first commit

---
+++ 8kyu/bin-to-Decimal.js
@@ -0,0 +1,8 @@
+// ❓ Завершите функцию, которая преобразует двоичное число (заданное в виде строки) в десятичное число.
+
+// ✅ SOLUTION:
+function binToDec(bin) {
+    return parseInt(bin,2)
+  }
+
+  console.log(binToDec(101));
