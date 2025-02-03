b37b72b7aa9423a4a11429d0567007330d8a50de
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2025-02-03 11:44:05 +0300

    first commit

---
+++ 8kyu/basic-data-types-String.js
@@ -0,0 +1,56 @@
+// ❓В javascript String — один из основных типов данных. Используйте ""или ''для объявления строковой переменной. Это могут быть буквы: a,b,c,A,B,C, цифры: 123..., различные символы: @#$%^&*, управляющие символы: \n,\tи т. д.
+
+// Строка может использовать операторы +, соединяющие две или более строк вместе.
+
+// Задача
+// миссия 1: Я создал три функции и определил несколько глобальных переменных. Выберите несколько переменных, которые могут составить имя функции, и верните их (обратите внимание, что заглавные и строчные буквы различаются).
+
+// Закончив работу, нажмите «Выполнить тесты», чтобы проверить, правильно ли работает ваш код.
+// миссия 2: После завершения миссии 1 вам следует нажать «Попытка», чтобы увидеть мои три вопроса, и написать ответ в функцииanswer1, answer2,answer3
+
+// Если все верно, нажмите «Отправить» еще раз, чтобы отправить свой код для прохождения этого ката.
+
+// ✅ SOLUTION:
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+function Dad() {
+  //select some variable to combine "Dad"
+  return `${d1 + a2 + d2}`;
+}
+function Bee() {
+  //select some variable to combine "Bee"
+  return `${b1 + e2 + e2}`;
+}
+function banana() {
+  //select some variable to combine "banana"
+  return `${b2 + a2 + n2 + a2 + n2 + a2}`;
+}
+
+//answer some questions if you finished works above
+function answer1() {
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer2() {
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3() {
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+
+//answer some questions if you finished works above
+function answer1(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer2(){
+  //the answer should be "yes" or "no"
+  return "no";
+}
+function answer3(){
+  //the answer should be "yes" or "no"
+  return "yes";
+}
+
+console.log(banana());
