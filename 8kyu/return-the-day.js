61091f046210e7092ce1b1b8bf12a805a82f88b2
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2025-02-12 00:35:13 +0300

    first commit

---
+++ 8kyu/return-the-day.js
@@ -0,0 +1,17 @@
+// ❓ Завершите функцию, которая возвращает день недели в соответствии с введенным числом:
+
+// 1возвращается"Sunday"
+// 2возвращается"Monday"
+// 3возвращается"Tuesday"
+// 4возвращается"Wednesday"
+// 5возвращается"Thursday"
+// 6возвращается"Friday"
+// 7возвращается"Saturday"
+// В противном случае возвращается"Wrong, please enter a number between 1 and 7"
+
+// ✅ SOLUTION:
+function whatday(num) { 
+return num === 1 ? "Sunday" : num === 2 ? "Monday" : num === 3 ? "Tuesday": num === 4 ? "Wednesday": num === 5? "Thursday": num === 6 ? "Friday" : num === 7 ? "Saturday" : "Wrong, please enter a number between 1 and 7"
+
+  }
+
