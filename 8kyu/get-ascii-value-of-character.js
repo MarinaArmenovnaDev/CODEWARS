202fd965eac36497ad2ec40e9e31edba865255e8
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2025-02-25 23:38:35 +0300

    fix

---
+++ 8kyu/get-ascii-value-of-character.js
@@ -0,0 +1,11 @@
+// ❓ Получить ASCII-код символа.
+
+// Таблицу ASCII можно найти на сайте http://www.asciitable.com/
+
+// ✅ SOLUTION:
+
+function getASCII(c){
+return c.charCodeAt()
+  }
+
+  console.log(getASCII("65"));
