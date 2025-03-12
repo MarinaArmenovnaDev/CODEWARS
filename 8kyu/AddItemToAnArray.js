859d3680f45e37bede52b3728c7044cc588a3f69
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2025-03-12 15:14:51 +0300

    first commit

---
+++ 8kyu/AddItemToAnArray.js
@@ -0,0 +1,12 @@
+// ❓ Добавьте значение "codewars" в массив websites.
+// После выполнения вашего кода массив websites должен == ["codewars"]
+
+// Массив веб-сайтов уже определен для вас с помощью следующего кода:
+
+// var websites = [];
+
+// ✅ SOLUITION:
+var websites = [];
+websites.push("codewars")
+
+console.log(websites);
