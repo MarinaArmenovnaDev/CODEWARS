b9b620c128dd4b77d0d8fe61459a5676566d1691
MarinaArmenovnaDev <awetisyanmarina@gmail.com>
2025-05-13 22:31:15 +0300

    first commit

---
+++ a.js
@@ -0,0 +1,12 @@
+//❓ You task to pass only this tests :
+
+// a == false
+// !a == false
+// a == !a
+
+// ✅ SOLUTION:
+const a = []
+
+console.log(a==false);
+console.log(!a==false);
+console.log(a==!a);
